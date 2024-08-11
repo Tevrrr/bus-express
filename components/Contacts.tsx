02968315f3a0436b53eda1a0297262a2b72c2a8f
Tevrrr <piskynov.vlad@gmail.com>
2024-08-11 12:47:09 +0300

    flex fix

--- components/Contacts.tsx
+++ components/Contacts.tsx
@@ -12,16 +12,14 @@ interface ContatctsProps {}
 const Contatcts: NextPage<ContatctsProps> = () => {
 	const value = useAnchorTarget('Контакты');
 	return (
-		<motion.div
-			{...value}
-			className='container px-4 pt-8 flex '>
-			<div className=' md:bg-neutral rounded-3xl w-full flex xl:flex-row flex-col items-center xl:justify-around md:p-10 gap-16 xl:gap-4 mb-4'>
+		<motion.div {...value} className='container px-4 pt-8 flex '>
+			<div className=' md:bg-neutral rounded-3xl w-full flex xl:flex-row flex-col  xl:justify-around md:p-10 gap-16 xl:gap-4 mb-4'>
 				<div className='flex flex-col max-w-[600px] w-full gap-8 xl:gap-24'>
 					<h3 className=' text-center'>Закажите наш звонок</h3>
 					<PanelOrderCall />
 				</div>
 				<Divider text='ИЛИ' />
-				<div className='flex flex-col max-w-[600px] w-full gap-8 xl:gap-24'>
+				<div className='flex flex-col max-w-[600px] w-full gap-8 xl:gap-[3.25rem]'>
 					<h3 className=' text-center'>Свяжитесь с нами сами</h3>
 					<ContactList />
 				</div>
--- components/UI/ContactList.tsx
+++ components/UI/ContactList.tsx
@@ -8,7 +8,7 @@ interface ContactListProps {}
 
 const ContactList: NextPage<ContactListProps> = () => {
 	return (
-		<div className=' flex flex-col gap-8 justify-between grow'>
+		<div className=' flex flex-col gap-10 justify-between  grow'>
 			<ContactCard phone='+380 99 258 37 13' />
 			{/* <ContactCard phone='+380 99 258 37 31' /> */}
 			<div className='flex justify-between  p-8 gap-4 bg-accent text-accent-content rounded-2xl'>
